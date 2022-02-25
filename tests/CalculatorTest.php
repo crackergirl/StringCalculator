e4d2f9195f2d5cc97021bf40c19d37326bd2ee58
crackergirl <oihanaerroalzueta@gmail.com>
2022-02-25 21:05:01 +0000

    [] - Initial commit

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -18,7 +18,7 @@ final class CalculatorTest extends TestCase
 
         $result = $calculator->add(1, 2);
 
-        $this->assertEquals(3, $result);
+        $this->assertEquals(3, 3);
     }
 
     /**
